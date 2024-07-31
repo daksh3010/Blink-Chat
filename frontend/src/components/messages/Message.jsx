78a98fb87123034dfc77cc27bb599f4e6dd73da6
Daksh <dakshkundu30102002@gmail.com>
2024-08-01 04:48:42 +0530

    2nd commit

---
+++ frontend/src/components/messages/Message.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Message() {
+  return (
+    <div className='chat chat-end'>
+        <div className='chat-image avatar'>
+            <div className='w-10 rounded-full'>
+                <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" alt="image" />
+
+            </div>
+        </div>
+        <div className={`chat-bubble text-white bg-blue-500 pb-2`}>
+            Hi!What's Up!!!!
+        </div>
+        <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>04:15</div>
+    </div>
+  )
+}
