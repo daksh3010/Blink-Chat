516ab73003f5395e116b9be0edbd12c81e899471
Daksh <dakshkundu30102002@gmail.com>
2024-08-01 22:11:25 +0530

    Socket Url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider = ({children}) => {
     const {authUser} = useAuthContext()
     useEffect(() => {
         if(authUser){
-            const socket = io('http://localhost:8000' , {
+            const socket = io('https://blink-chat-w623.onrender.com' , {
                 query : {
                     userId: authUser._id,
                 }
